515eb889f1c7aea8ffbc900b152b3cc28cc5285c
CrustaceanKing <swald@ucsd.edu>
2022-05-22 12:11:57 -0700

    Added Snippet Tests for Lab 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -98,4 +98,38 @@ public class MarkdownParseTest {
         ArrayList<String> links = markParse2.getLinks(content);
         assertEquals(compareString, links);
     }
+
+    @Test
+    public void testSnippet1() throws IOException{
+        ArrayList<String> compareString = new ArrayList<String>();
+        compareString.add("'google.com");
+        compareString.add("google.com");
+        compareString.add("ucsd.edu");
+        Path fileName = Path.of("Snippet-1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = markParse2.getLinks(content);
+        assertEquals(compareString, links);
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        ArrayList<String> compareString = new ArrayList<String>();
+        compareString.add("a.com");
+        compareString.add("a.com(())");
+        compareString.add("example.com");
+        Path fileName = Path.of("Snippet-2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = markParse2.getLinks(content);
+        assertEquals(compareString, links);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        ArrayList<String> compareString = new ArrayList<String>();
+        compareString.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        Path fileName = Path.of("Snippet-3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = markParse2.getLinks(content);
+        assertEquals(compareString, links);
+    }
 }
